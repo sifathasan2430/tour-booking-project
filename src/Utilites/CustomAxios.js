efe33dc8fc4eeda41b325a36a0f11698ce3dbe2b
sifat hasan <sifatshasan@gmail.com>
2025-06-17 18:35:10 +0600

    customaxios build  and api protected

--- src/Utilites/CustomAxios.js
+++ src/Utilites/CustomAxios.js
@@ -2,20 +2,25 @@ import axios from "axios";
 import auth from "../FireBase/firebase.config";
 import { onAuthStateChanged } from "firebase/auth";
 
+
 const axiosLoader=axios.create({
     baseURL:"https://tour-management-server-side.vercel.app",
       withCredentials: true,
 })
 
-   onAuthStateChanged(auth, (user) => {
-  if (user) {
-        axiosLoader.interceptors.request.use((config)=>{
-            config.headers.Authorization=`Bearer ${user.accessToken}`
-            return config
-        })
-  } else {
-    return null
-  }
-});
+   onAuthStateChanged(auth, async(user) => {
+
+
+    axiosLoader.interceptors.request.use(async (config) => {
+      const token = await user.getIdToken();
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+   })
+   
+  
+        
 
 export default axiosLoader
